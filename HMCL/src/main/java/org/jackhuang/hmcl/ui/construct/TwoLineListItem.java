2b6b99399d39a04e979014702ddd6ab61376dbbc
Burning_TNT <pangyl08@163.com>
2023-08-26 18:37:13 +0800

    Developing

--- HMCL/src/main/java/org/jackhuang/hmcl/ui/construct/TwoLineListItem.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/ui/construct/TwoLineListItem.java
@@ -17,12 +17,7 @@
  */
 package org.jackhuang.hmcl.ui.construct;
 
-import javafx.animation.Animation;
-import javafx.animation.Interpolator;
 import javafx.animation.TranslateTransition;
-import javafx.application.Platform;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
@@ -36,24 +31,19 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 import org.jackhuang.hmcl.setting.ConfigHolder;
 import org.jackhuang.hmcl.ui.FXUtils;
-import org.jackhuang.hmcl.util.Lang;
 import org.jackhuang.hmcl.util.javafx.BindingMapping;
 
 import java.util.List;
 import java.util.function.Function;
-import java.util.logging.Level;
-
-import static org.jackhuang.hmcl.util.Logging.LOG;
 
 public class TwoLineListItem extends VBox {
     private static final String DEFAULT_STYLE_CLASS = "two-line-list-item";
     private static final double TITLE_PART = 0.7D;
 
-    private final class TagChangeListener implements ListChangeListener<String> {
+    private static final class TagChangeListener implements ListChangeListener<String> {
         private final List<Node> list;
         private final Function<String, Node> mapper;
 
@@ -85,8 +75,6 @@ public class TwoLineListItem extends VBox {
                     }
                 }
             }
-
-            Platform.runLater(TwoLineListItem.this::layout);
         }
     }
 
@@ -94,21 +82,11 @@ public class TwoLineListItem extends VBox {
     private final ObservableList<String> tags = FXCollections.observableArrayList();
     private final StringProperty subtitle = new SimpleStringProperty(this, "subtitle");
 
-    private final Label titleLabel;
-    private final HBox tagBox;
-    private final Label subTitleLabel;
-
-    private final BooleanProperty titleTranslatePlaying = new SimpleBooleanProperty(false);
-    private final BooleanProperty tagTranslatePlaying = new SimpleBooleanProperty(false);
-    private final BooleanProperty subTitleTranslatePlaying = new SimpleBooleanProperty(false);
-
     public TwoLineListItem(String titleString, String subtitleString) {
         this();
 
         title.set(titleString);
         subtitle.set(subtitleString);
-
-        Platform.runLater(this::reLayout);
     }
 
     public TwoLineListItem() {
@@ -117,94 +95,114 @@ public class TwoLineListItem extends VBox {
         HBox firstLine = new HBox();
         firstLine.getStyleClass().add("first-line");
 
-        {
-            Pane titlePane = new Pane();
-            titlePane.getStyleClass().add("title-pane");
+        Pane titlePane = new Pane();
+        titlePane.getStyleClass().add("title-pane");
 
-            titleLabel = new Label();
-            titleLabel.getStyleClass().add("title");
-            titleLabel.textProperty().bind(BindingMapping.of(title).map(s -> s + " | " + s));
+        HBox titleBox = new HBox();
 
-            Rectangle titleClip = new Rectangle(0, 0, titleLabel.getWidth() / 2 - 10, titleLabel.getHeight());
-            titleClip.widthProperty().bind(BindingMapping.of(titleLabel.widthProperty()).map(w -> w.doubleValue() / 2 - 10));
-            titlePane.setClip(titleClip);
-            titlePane.getChildren().setAll(titleLabel);
+        Label titleLabelA = new Label();
+        titleLabelA.getStyleClass().add("title");
+        titleLabelA.textProperty().bind(title);
 
-            Pane tagLabelsPane = new Pane();
-            tagLabelsPane.getStyleClass().add("tag-pane");
+        Label titleLabelSplit = new Label();
+        titleLabelSplit.getStyleClass().add("title");
+        titleLabelSplit.setText("|");
 
-            tagBox = new HBox();
-            tags.addListener(new TagChangeListener(tagBox.getChildren(), txt -> {
-                Label tagLabel = new Label();
-                tagLabel.getStyleClass().add("tag");
-                tagLabel.setText(txt);
-                HBox.setMargin(tagLabel, new Insets(0, 8, 0, 0));
-                return tagLabel;
-            }));
+        Label titleLabelB = new Label();
+        titleLabelB.getStyleClass().add("title");
+        titleLabelB.textProperty().bind(title);
 
-            Rectangle tagClip = new Rectangle(0, 0, tagBox.getWidth() / 2 - 2, tagBox.getHeight());
-            tagClip.widthProperty().bind(BindingMapping.of(tagBox.widthProperty()).map(w -> w.doubleValue() / 2 - 2));
-            tagLabelsPane.setClip(tagClip);
-            tagLabelsPane.getChildren().setAll(tagBox);
+        titleBox.getChildren().setAll(titleLabelA, titleLabelSplit, titleLabelB);
+        titlePane.getChildren().setAll(titleBox);
+        titlePane.maxWidthProperty().bind(this.widthProperty().multiply(TITLE_PART));
+        titlePane.prefWidthProperty().bind(titleLabelA.widthProperty());
+        titlePane.prefHeightProperty().bind(titleLabelA.heightProperty());
 
-            firstLine.getChildren().setAll(titlePane, tagLabelsPane);
-        }
+        Pane tagPane = new Pane();
+        tagPane.getStyleClass().add("tag-pane");
 
-        Pane secondLine = new Pane();
-        {
-            subTitleLabel = new Label();
-            subTitleLabel.getStyleClass().add("subtitle");
-            subTitleLabel.textProperty().bind(BindingMapping.of(subtitle).map(s -> s + " | " + s));
+        HBox tagBox = new HBox();
+        tags.addListener(new TagChangeListener(tagBox.getChildren(), txt -> {
+            Label tagLabel = new Label();
+            tagLabel.getStyleClass().add("tag");
+            tagLabel.setText(txt);
+            HBox.setMargin(tagLabel, new Insets(0, 8, 0, 0));
+            return tagLabel;
+        }));
 
-            Rectangle subTitleClip = new Rectangle(0, 0, subTitleLabel.getWidth() / 2 - 2, 80000);
-            subTitleClip.widthProperty().bind(BindingMapping.of(subTitleLabel.widthProperty()).map(w -> w.doubleValue() / 2 - 2));
-            secondLine.setClip(subTitleClip);
+        tagPane.getChildren().setAll(tagBox);
 
-            secondLine.getChildren().setAll(subTitleLabel);
-        }
+        firstLine.getChildren().setAll(titlePane, tagPane);
+        HBox.setHgrow(tagPane, Priority.ALWAYS);
 
-        getChildren().setAll(firstLine, secondLine);
+        Pane subTitlePane = new Pane();
 
-        FXUtils.onChangeAndOperate(subtitle, subtitleString -> {
-            if (subtitleString == null) getChildren().setAll(firstLine);
-            else getChildren().setAll(firstLine, secondLine);
-        });
+        Label subTitleLabel = new Label();
+        subTitleLabel.getStyleClass().add("subtitle");
+        subTitleLabel.textProperty().bind(BindingMapping.of(subtitle).map(t -> t + " | " + t));
+
+        subTitlePane.getChildren().setAll(subTitleLabel);
+        subTitlePane.maxWidthProperty().bind(this.widthProperty());
+        subTitlePane.prefWidthProperty().bind(BindingMapping.of(subTitleLabel.widthProperty()).map(w -> w.doubleValue() / 2));
 
         getStyleClass().add(DEFAULT_STYLE_CLASS);
 
-        TranslateTransition titleTranslateTransition = new TranslateTransition(Duration.seconds(0.1D), titleLabel);
-        titleTranslateTransition.setFromX(0);
-        titleTranslateTransition.durationProperty().bind(BindingMapping.of(titleLabel.widthProperty()).map(w -> Duration.seconds((w.doubleValue() / 2D + 1) / 30D)));
-        titleTranslateTransition.toXProperty().bind(BindingMapping.of(titleLabel.widthProperty()).map(w -> -w.doubleValue() / 2D - 1));
-        titleTranslateTransition.setInterpolator(Interpolator.LINEAR);
-        titleTranslateTransition.setCycleCount(Animation.INDEFINITE);
-        titleTranslatePlaying.addListener(generateTranslateListener(titleTranslateTransition));
+//        BooleanProperty titleTranslatePlaying = new SimpleBooleanProperty(false);
+//        {
+//            TranslateTransition titleTranslateTransition = new TranslateTransition(Duration.seconds(0.1D), titleLabel);
+//            titleTranslateTransition.setFromX(0);
+//            titleTranslateTransition.durationProperty().bind(BindingMapping.of(titleLabel.widthProperty()).map(w -> Duration.seconds((w.doubleValue() / 2D + 1) / 30D)));
+//            titleTranslateTransition.toXProperty().bind(BindingMapping.of(titleLabel.widthProperty()).map(w -> -w.doubleValue() / 2D - 1));
+//            titleTranslateTransition.setInterpolator(Interpolator.LINEAR);
+//            titleTranslateTransition.setCycleCount(Animation.INDEFINITE);
+//            titleTranslatePlaying.addListener(generateTranslateListener(titleTranslateTransition));
+//        }
+//
+//        BooleanProperty tagTranslatePlaying = new SimpleBooleanProperty(false);
+//        {
+//            TranslateTransition tagTranslateTransition = new TranslateTransition(Duration.seconds(0.1D), tagBox);
+//            tagTranslateTransition.setFromX(0);
+//            tagTranslateTransition.durationProperty().bind(BindingMapping.of(tagBox.widthProperty()).map(w -> Duration.seconds(w.doubleValue() / 2D / 30D)));
+//            tagTranslateTransition.toXProperty().bind(BindingMapping.of(tagBox.widthProperty()).map(w -> -w.doubleValue() / 2D));
+//            tagTranslateTransition.setInterpolator(Interpolator.LINEAR);
+//            tagTranslateTransition.setCycleCount(Animation.INDEFINITE);
+//            tagTranslatePlaying.addListener(generateTranslateListener(tagTranslateTransition));
+//        }
+//
+//        BooleanProperty subTitleTranslatePlaying = new SimpleBooleanProperty(false);
+//        {
+//            TranslateTransition subTitleTranslateTransition = new TranslateTransition(Duration.seconds(0.1D), subTitleLabel);
+//            subTitleTranslateTransition.setFromX(0);
+//            subTitleTranslateTransition.durationProperty().bind(BindingMapping.of(subTitleLabel.widthProperty()).map(w -> Duration.seconds(w.doubleValue() / 2D / 30D)));
+//            subTitleTranslateTransition.toXProperty().bind(BindingMapping.of(subTitleLabel.widthProperty()).map(w -> (-w.doubleValue() - 8) / 2D));
+//            subTitleTranslateTransition.setInterpolator(Interpolator.LINEAR);
+//            subTitleTranslateTransition.setCycleCount(Animation.INDEFINITE);
+//            subTitleTranslatePlaying.addListener(generateTranslateListener(subTitleTranslateTransition));
+//        }
 
-        TranslateTransition tagTranslateTransition = new TranslateTransition(Duration.seconds(0.1D), tagBox);
-        tagTranslateTransition.setFromX(0);
-        tagTranslateTransition.durationProperty().bind(BindingMapping.of(tagBox.widthProperty()).map(w -> Duration.seconds(w.doubleValue() / 2D / 30D)));
-        tagTranslateTransition.toXProperty().bind(BindingMapping.of(tagBox.widthProperty()).map(w -> -w.doubleValue() / 2D));
-        tagTranslateTransition.setInterpolator(Interpolator.LINEAR);
-        tagTranslateTransition.setCycleCount(Animation.INDEFINITE);
-        tagTranslatePlaying.addListener(generateTranslateListener(tagTranslateTransition));
-
-        TranslateTransition subTitleTranslateTransition = new TranslateTransition(Duration.seconds(0.1D), subTitleLabel);
-        subTitleTranslateTransition.setFromX(0);
-        subTitleTranslateTransition.durationProperty().bind(BindingMapping.of(subTitleLabel.widthProperty()).map(w -> Duration.seconds(w.doubleValue() / 2D / 30D)));
-        subTitleTranslateTransition.toXProperty().bind(BindingMapping.of(subTitleLabel.widthProperty()).map(w -> (-w.doubleValue() - 8) / 2D));
-        subTitleTranslateTransition.setInterpolator(Interpolator.LINEAR);
-        subTitleTranslateTransition.setCycleCount(Animation.INDEFINITE);
-        subTitleTranslatePlaying.addListener(generateTranslateListener(subTitleTranslateTransition));
-
-        Rectangle mainClip = new Rectangle(0, 0, this.getWidth(), this.getHeight());
-        mainClip.widthProperty().bind(this.widthProperty());
-        mainClip.heightProperty().bind(this.heightProperty());
-        this.setClip(mainClip);
+//        Rectangle mainClip = new Rectangle(0, 0, this.getWidth(), this.getHeight());
+//        mainClip.widthProperty().bind(this.widthProperty());
+//        mainClip.heightProperty().bind(this.heightProperty());
+//        this.setClip(mainClip);
 
         this.minWidthProperty().set(0);
         HBox.setHgrow(this, Priority.SOMETIMES);
 
-        Lang.immutableListOf(title, subtitle, this.widthProperty()).forEach(o -> o.addListener(observable -> Platform.runLater(this::reLayout)));
+        FXUtils.onChangeAndOperate(subtitle, subtitleString -> {
+            if (subtitleString == null) {
+                getChildren().setAll(firstLine);
+            } else {
+                getChildren().setAll(firstLine, subTitlePane);
+            }
+        });
+
+        titlePane.setStyle("-fx-border-color: red;");
+        titleLabelA.setStyle("-fx-border-color: yellow;");
+        titleLabelSplit.setStyle("-fx-border-color: yellow;");
+        titleLabelB.setStyle("-fx-border-color: yellow;");
+        tagPane.setStyle("-fx-border-color: blue;");
+        tagBox.setStyle("-fx-border-color: green;");
+        this.setStyle("-fx-border-color: black;");
     }
 
     private static ChangeListener<Boolean> generateTranslateListener(TranslateTransition translateTransition) {
@@ -223,32 +221,6 @@ public class TwoLineListItem extends VBox {
                 }
             };
         }
-    }
-
-    private void reLayout() {
-        try {
-            this.layout();
-        } catch (NullPointerException npe) {
-            LOG.log(Level.WARNING, "For some unknown reasons, invoking this::layout too fast may produce NullPointerException.", npe);
-        }
-
-        double titleMaxWidth = tags.size() == 0 ? this.getWidth() : this.getWidth() * TITLE_PART;
-        double titleWidth = Math.min(titleLabel.getWidth() / 2, titleMaxWidth);
-        double tagMaxWidth = this.getWidth() - titleWidth - this.paddingProperty().get().getLeft() - this.paddingProperty().get().getRight() - 5;
-        double tagWidth = Math.min(tagBox.getWidth() / 2, tagMaxWidth);
-
-        FXUtils.setLimitWidth((Pane) titleLabel.getParent(), titleWidth);
-        Rectangle titleClip = new Rectangle(0, 0, titleWidth - 7, titleLabel.getHeight());
-        titleLabel.getParent().setClip(titleClip);
-
-        titleTranslatePlaying.set(titleLabel.getWidth() / 2 > titleMaxWidth);
-
-        Rectangle tagClip = new Rectangle(0, 0, tagWidth - 2, tagBox.getHeight());
-        tagBox.getParent().setClip(tagClip);
-
-        tagTranslatePlaying.set(tagBox.getWidth() / 2 > tagMaxWidth);
-
-        subTitleTranslatePlaying.set(subTitleLabel.getWidth() / 2 > this.getWidth());
     }
 
     public String getTitle() {
